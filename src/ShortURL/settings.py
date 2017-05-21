4d0fdf4cd5833cd52471d7b6b88d80eb22c0cfff
Piyush Gandhi <p.gandhi2802@yahoo.in>
2017-05-21 13:36:17 +0530

    Basic URL Routing Configured

--- src/ShortURL/settings.py
+++ src/ShortURL/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Short.apps.ShortConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
